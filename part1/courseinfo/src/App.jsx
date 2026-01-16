a9d33a5bc39f74afc815a2c59053cc919c3d08bb
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-01-16 02:52:43 -0600

    Ejercicio 1.3: Información del Curso, paso 3 - Realizado

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,26 +1,33 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
       <Header course={course} />
       <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
+        part1={part1}
+        part2={part2}
+        part3={part3}
       />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Total part1={part1} part2={part2} part3={part3}/>
     </div>
   )
 }
 
 const Header = (props) =>{
+  console.log(props)
   return(
     <div>
       <h1>{props.course}</h1>
@@ -30,28 +37,31 @@ const Header = (props) =>{
 }
 
 const Content = (props) =>{
+  console.log(props)
   return(
     <div>
-      <Part name={props.part1} exercise={props.exercises1}/>
-      <Part name={props.part2} exercise={props.exercises2}/>
-      <Part name={props.part3} exercise={props.exercises3}/>
+      <Part parte={props.part1}/>
+      <Part parte={props.part2}/>
+      <Part parte={props.part3}/>
     </div>
   )
 
 }
 
 const Part = (props) =>{
+  console.log(props)
   return(
     <div>
-      <p>{props.name} {props.exercise}</p> <br />
+      <p>{props.parte.name} {props.parte.exercises}</p> <br />
     </div>
   )
 }
 
 const Total = (props) =>{
+  console.log(props)
   return(
     <div>
-      <p>{props.exercises1 + props.exercises2 + props.exercises3}</p>
+      <p>{props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
     </div>
   )
 }
