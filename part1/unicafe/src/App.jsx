48027bbfc2af7e953ce6415ecab0004f62745cf0
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-01-17 17:47:33 -0600

    Ejercicio 1.7: unicafe, paso 2 - Realizado. Implementación de Total, promedio y reseñas positivas

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,6 +1,10 @@
 import { useState } from 'react'
 
 const App = () => {
+
+  let average = 0
+  let positive = 0
+  
   const strings = {
     feedback: "give feedback",
     subtitle: "statics"
@@ -8,7 +12,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  
   const handleGoodClick = () =>{
     setGood(good + 1)
   }
@@ -21,6 +25,13 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const total = good + neutral + bad
+
+  if (total !== 0) {
+    average = (good * 1 + neutral * 0 + bad * -1) / total
+    positive = (good / total) * 100
+  }
+
   return (
     <div>
       <Header feedback={strings.feedback}/>
@@ -28,10 +39,13 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text='neutral'/>
       <Button handleClick={handleBadClick} text='bad'/>
       <Subtitle subtitle={strings.subtitle}/>
-      <Statics text='good' tag={good}/>
-      <Statics text='neutral' tag={neutral}/>
-      <Statics text='bad' tag={bad}/>
-      
+      <Statics text='Good' tag={good}/>
+      <Statics text='Neutral' tag={neutral}/>
+      <Statics text='Bad' tag={bad}/>
+      <Total text='Total' total={total}/>
+      <Average text='Average' average={average}/>
+      <Positive text='Positive' positive={positive + "%"}/>
+
     </div>
   )
 }
@@ -58,6 +72,24 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>
     {text}
   </button>
+)
+
+const Total = ({total, text}) => (
+  <div>
+    <p>{text} = {total}</p>
+  </div>
+)
+
+const Average = ({average, text}) => (
+  <div>
+    <p>{text} = {average}</p>
+  </div>
+)
+
+const Positive = ({text, positive}) => (
+  <div>
+    <p>{text} = {positive}</p>
+  </div>
 )
 
 export default App
