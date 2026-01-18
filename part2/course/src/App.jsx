d89af93251b0367cde7e7062a68b28c4c53cf1f8
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-01-18 03:12:01 -0600

    Ejercicio 2.1: Información del curso paso 6 - Realizado. Se dividieron en componentes

---
+++ part2/course/src/App.jsx
@@ -0,0 +1,55 @@
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+const Course = ({course}) => {
+  return(
+    <div>
+      <Header name={course.name}/>
+      <Content parts={course.parts}/>
+    </div>
+  )
+}
+
+const Header = ({name}) => (
+  <div>
+    <h1>{name}</h1>
+  </div>
+)
+
+const Content = ({parts}) => (
+  <div>
+    {parts.map(part => 
+      <Part key={part.id} part={part} />
+    )}
+  </div>
+)
+
+const Part = ({part}) => (
+  <div>
+    {part.name} {part.exercises}
+  </div>
+)
+export default App
