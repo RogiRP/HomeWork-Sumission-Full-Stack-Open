fe948b20f213eade794bb6def48bda5b43811bed
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-03-31 22:18:28 -0600

    2.8: La Agenda Telefónica Paso 3 - COMPLETADO

--- part2/Agenda/src/App.tsx
+++ part2/Agenda/src/App.tsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas' ,
+      number: '9993321112'
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  
 
   const handleNameChange = (event) =>{
 
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) =>{
     setNewName(event.target.value)
   }
 
@@ -24,13 +32,16 @@ const App = () => {
     }
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     setPersons(persons.concat(personObject))
     setNewName('')
+    setNewNumber('')
   }
 
+  
 
   return (
     <div>
@@ -40,13 +51,16 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       
       {persons.map(person =>(
-        <div key={person.name}>{person.name}</div>
+        <div key={person.name}>{person.name}{person.number}</div>
       ))}
       
     </div>
