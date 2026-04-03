b3414036acfd558029c51cb54622daef03982e90
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-04-02 23:29:04 -0600

    2.12: La Agenda Telefónica paso 7 - COMPLETADO

--- part2/Agenda/src/App.jsx
+++ part2/Agenda/src/App.jsx
@@ -1,18 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '9993321112' },
-    { name: 'Ada Lovelace', number: '1241244566' },
-    { name: 'Dan Abramov', number: '114214564365' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -43,9 +46,13 @@ const App = () => {
       number: newNumber
     }
 
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    setNewNumber('')
+    axios.post('http://localhost:3001/persons', personObject).then(
+      response => {
+        setPersons(persons.concat(response.data))
+        setNewName('')
+        setNewNumber('')
+      }
+    )
   }
 
   const personsToShow = persons.filter(person =>
