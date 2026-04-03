bc98ca27e5f88b46b981f4337625f4ae18c6965d
roger_leo28hotmail.com <roger_leo28@hotmail.com>
2026-04-02 19:52:16 -0600

    2.10: La Agenda Telefónica Paso 5 - Completada y pasada a jsx

---
+++ part2/Agenda/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState } from 'react'
+import Filter from './components/Filter'
+import PersonForm from './components/PersonForm'
+import Persons from './components/Persons'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '9993321112' },
+    { name: 'Ada Lovelace', number: '1241244566' },
+    { name: 'Dan Abramov', number: '114214564365' }
+  ])
+
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setFilter] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+
+    const existingPerson = persons.some(
+      person => person.name === newName
+    )
+
+    if (existingPerson) {
+      alert(`${newName} is already added to phonebook`)
+      return
+    }
+
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const personsToShow = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+
+      <Filter
+        filter={filter}
+        handleFilterChange={handleFilterChange}
+      />
+
+      <h2>Add a new</h2>
+
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+
+      <h2>Numbers</h2>
+
+      <Persons personsToShow={personsToShow} />
+    </div>
+  )
+}
+
+export default App
